81b6e668f71c70d6b65562283b3bc5feb85c5a70
Sithu Sharma <sithusharma@Sithus-iMac.local>
2024-03-13 23:17:23 -0400

    fixed errors in project.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,5 @@
 import React from 'react'
 import ProjectItem from "../components/ProjectItem";
-import movie from '../assets/movie.jpeg'
-import emotion from '../assets/emotion.jpeg'
-import plane from '../assets/plane.jpeg'
-import web from '../assets/web.jpeg'
-import weather from '../assets/weather.jpeg'
-import find from '../assets/find.jpg'
 import "../styles/Project.css"
 import {ProjectList} from "../helpers/ProjectList"
 

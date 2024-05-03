97b2b66c48be92eac6e6d647e2f32138fb2d9247
sithu <sithusharma@vt.edu>
2024-05-03 15:57:14 -0400

    changed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
 import ReorderIcon from '@material-ui/icons/Reorder';
 import { useEffect, useState } from "react";
@@ -11,28 +11,28 @@ function Navbar() {
     useEffect(() => {
         setExpandNavbar(false);
     }, [location]);
-  return (
-    <div className="navbar" id ={expandNavbar ? "open" : "close"}>
-        <div className="toggleButton">
-            <button 
-                onClick={() => {
-                    setExpandNavbar((prev) => !prev);
+    return (
+        <div className="navbar" id={expandNavbar ? "open" : "close"}>
+            <div className="toggleButton">
+                <button
+                    onClick={() => {
+                        setExpandNavbar((prev) => !prev);
                     }}>
-                <ReorderIcon/>
-            </button>
+                    <ReorderIcon />
+                </button>
 
-        </div>
-        <div className="links">
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/experiences">Experiences</Link>
-            <Link to="/resume">Resume</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/video">Video Project</Link>
+            </div>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/experiences">Experiences</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/blog">Podcast</Link>
+                <Link to="/video">Video Project</Link>
 
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar;
